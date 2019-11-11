f63e3f4c2c3862414b9e793ac2f917bad33ba8aa
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-11 22:44:40 +0200

    adding forms

---
+++ eCommerce/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
+
+def home_page(request):
+    context = {
+        "title":"hello world!",
+        "content": "Welcome To Home Page"
+    }
+    return render(request,'home_page.html',context)
+
+def about_page(request):
+    context = {
+        "title":"About Page",
+        "content": "Welcome To About Page"
+    }
+    return render(request,'home_page.html',context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title":"Contact",
+        "content": "Welcome To Contact Page",
+        "form":contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request,'contact/view.html',context)
