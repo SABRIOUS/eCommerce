48d88eac752e88f57d94b4b134e95260fbe29d79
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-12 01:12:31 +0200

    adding contact and working with loggin

--- eCommerce/views.py
+++ eCommerce/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
     context = {
@@ -26,3 +26,19 @@ def contact_page(request):
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
     return render(request,'contact/view.html',context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    print("User Logged In ... ")
+    print(request.user.is_authenticated)
+    context = {
+        "form":form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request,"auth/login.html",context)
+
+
+def register_page(request):
+    return render(request,"auth/register.html",{})
