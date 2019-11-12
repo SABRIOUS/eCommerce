ae6d48298740995bf6fe84f8c5deb15d5382cec8
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-12 13:29:19 +0200

    adding register forms

--- eCommerce/views.py
+++ eCommerce/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login,get_user_model
 from django.http import HttpResponse
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm,RegisterForm
 
 def home_page(request):
     context = {
         "title":"hello world!",
         "content": "Welcome To Home Page"
     }
+    if request.user.is_authenticated:
+        context["premium_content"] = "YEAHHHHHHHHHH"
     return render(request,'home_page.html',context)
 
 def about_page(request):
@@ -30,15 +33,37 @@ def contact_page(request):
 
 def login_page(request):
     form = LoginForm(request.POST or None)
-    print("User Logged In ... ")
-    print(request.user.is_authenticated)
     context = {
         "form":form
     }
     if form.is_valid():
         print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request,username=username,password=password)
+        print(user)
+        if user is not None:
+            login(request,user)
+            #clear the form
+            # context['form'] = LoginForm()
+            # Redirect to a sccess page.
+            return redirect("/")
+        else:
+            print("Error... ")
+            # Return and 'invalid login' error message
     return render(request,"auth/login.html",context)
 
-
+User = get_user_model()
 def register_page(request):
-    return render(request,"auth/register.html",{})
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form":form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        email = form.cleaned_data.get("email")
+        new_user = User.objects.create_user(username,email,password)
+        print(new_user)
+    return render(request,"auth/register.html",context)
