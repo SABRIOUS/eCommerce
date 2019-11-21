129aff5a947ff204e356791055a5331ad9eedde2
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-21 19:41:10 +0200

    finished search

--- products/models.py
+++ products/models.py
@@ -47,7 +47,10 @@ class ProductManger(models.Manager):
     def search(self,query):
         lookups = (Q(title__icontains=query)|
                   Q(description__icontains=query)|
-                  Q(price__icontains=query))
+                  Q(price__icontains=query)|
+                  Q(tag__title__icontains=query))
+
+
         return self.filter(lookups).distinct()
 
 class Product(models.Model):
