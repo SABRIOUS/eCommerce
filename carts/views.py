78b18703ca624da9459d54f1ead3a061bae2eed0
SABRIOUS <ahmedsabrious@gmail.com>
2019-11-25 17:43:35 +0200

    sender

--- carts/views.py
+++ carts/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
+
 from .models import Cart
-# Create your views here.
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id",None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print("Cart ID Exists")
-        cart_obj =qs.first()
-        if request.user.is_authenticated and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-    return render(request,"carts/home.html",{})
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for i in products:
+        total += i.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
+    return render(request, "carts/home.html", {})
